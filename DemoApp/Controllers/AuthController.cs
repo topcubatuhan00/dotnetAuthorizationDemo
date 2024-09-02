ed39a59ba841a72ae334259920c5cea1fa5a158e
Batuhan Topçu <topcubatuhan00@hotmail.com>
2024-09-02 15:03:02 +0300

    inital commit

---
+++ DemoApp/Controllers/AuthController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace DemoApp.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class AuthController : ControllerBase
+{
+    string signingKey = "BuBenimCokDahaUzunVeGuvenliSigningKeyDegerim0123456789";
+    [HttpGet]
+    public string Login(string userName, string password)
+    {
+        var claims = new[]
+        {
+        new Claim(ClaimTypes.Name, userName),
+        new Claim(JwtRegisteredClaimNames.Email, userName)
+    };
+
+        
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signingKey));
+        var securityCreds = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var jwtSecurityToken = new JwtSecurityToken(
+                issuer: "https://topcubatuhan.com",
+                audience: "BuBenimKullandigimAudienceDegeri",
+                claims: claims,
+                expires: DateTime.Now.AddDays(15),
+                notBefore: DateTime.Now,
+                signingCredentials: securityCreds
+            );
+        var token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+        return token;
+    }
+
+    [HttpGet("ValidateToken")]
+    public bool ValidateToken(string token)
+    {
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signingKey));
+        try
+        {
+            JwtSecurityTokenHandler handler = new();
+            handler.ValidateToken(token, new TokenValidationParameters() {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = securityKey,
+                ValidateLifetime = true,
+                ValidateAudience = false,
+                ValidateIssuer = false
+            }, out SecurityToken validatedToken);
+
+            var jwtToken = (JwtSecurityToken)validatedToken;
+            var claims = jwtToken.Claims.ToList();
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+
+    }
+}
